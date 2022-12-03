1648713d8e57646143b8251b5c1aec3aabff2f85
Joe Barnett <joebarnett876@gmail.com>
2022-12-03 10:59:18 +0000

    day3

--- src/puzzles/day3/src/main.rs
+++ src/puzzles/day3/src/main.rs
@@ -1,5 +1,7 @@
 use std::collections::HashSet;
 
+use itertools::Itertools;
+
 fn item_priority(item: char) -> u32 {
     match item {
         'a'..='z' => (item as u32) - 96,
@@ -8,29 +10,40 @@ fn item_priority(item: char) -> u32 {
     }
 }
 
-fn compartment_members(compartment: &str) -> HashSet<char> {
-    HashSet::from_iter(compartment.chars())
+fn single_common_character<'a>(strings: impl Iterator<Item = &'a str>) -> char {
+    let sets = strings.map(|s| HashSet::from_iter(s.chars()));
+    let common_chars: HashSet<_> = sets
+        .reduce(|acc, set| HashSet::from_iter(acc.intersection(&set).cloned()))
+        .unwrap_or_else(|| panic!("unexpected empty group of sets"));
+    if common_chars.len() > 1 {
+        panic!("found multiple common characters");
+    }
+    common_chars
+        .into_iter()
+        .next()
+        .unwrap_or_else(|| panic!("found no common character"))
 }
 
 fn main() {
     let input = utils::read_input();
+
     let part_1_answer = input
         .lines()
         .map(|line| {
             let (first_compartment, second_compartment) = line.split_at(line.len() / 2);
-            let first_compartment_members = compartment_members(first_compartment);
-            let second_compartment_members = compartment_members(second_compartment);
-            let misplaced_items: Vec<_> = first_compartment_members
-                .intersection(&second_compartment_members)
-                .collect();
-            if misplaced_items.len() > 1 {
-                panic!("found multiple misplaced items");
-            }
-            let misplaced_item = misplaced_items
-                .first()
-                .unwrap_or_else(|| panic!("found no misplaced item"));
-            item_priority(**misplaced_item)
+            let misplaced_item =
+                single_common_character(vec![first_compartment, second_compartment].into_iter());
+            item_priority(misplaced_item)
         })
         .sum::<u32>();
+
+    let part_2_answer = input
+        .lines()
+        .chunks(3)
+        .into_iter()
+        .map(|elf_group| item_priority(single_common_character(elf_group)))
+        .sum::<u32>();
+
     println!("part 1: {}", part_1_answer);
+    println!("part 2: {}", part_2_answer);
 }
