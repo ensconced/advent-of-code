a7ab70e729db69a075904c8971438a39744f8089
Joe Barnett <joebarnett876@gmail.com>
2022-12-08 22:11:46 +0000

    day8 part 1

--- src/puzzles/day8/src/main.rs
+++ src/puzzles/day8/src/main.rs
@@ -13,8 +13,8 @@ fn transpose<T: Copy>(grid: Matrix<T>) -> Matrix<T> {
         .collect()
 }
 
-fn rotate<T: Copy>(grid: Matrix<T>, n: u32) -> Matrix<T> {
-    (0..n).fold(grid, |acc, _| {
+fn rotate<T: Copy>(grid: &Matrix<T>, n: u32) -> Matrix<T> {
+    (0..n).fold(grid.clone(), |acc, _| {
         transpose(acc)
             .into_iter()
             .map(|row| row.into_iter().rev().collect())
@@ -22,7 +22,7 @@ fn rotate<T: Copy>(grid: Matrix<T>, n: u32) -> Matrix<T> {
     })
 }
 
-fn running_max(line: &Vec<i32>) -> Vec<i32> {
+fn max_blocking_tree_heights(line: &Vec<i32>) -> Vec<i32> {
     let mut result = vec![];
     let mut max = -1;
     for tree in line {
@@ -32,12 +32,8 @@ fn running_max(line: &Vec<i32>) -> Vec<i32> {
     result
 }
 
-fn leftwards_blockers_height(grid: &Matrix<i32>) -> Matrix<i32> {
-    grid.iter().map(running_max).collect()
-}
-
 fn visibility_from_left(grid: &Matrix<i32>) -> Matrix<bool> {
-    let blockers = leftwards_blockers_height(grid);
+    let blockers: Vec<_> = grid.iter().map(max_blocking_tree_heights).collect();
     grid.iter()
         .enumerate()
         .map(|(row_idx, row)| {
@@ -60,32 +56,22 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
-    let visible_from_left = visibility_from_left(&grid);
-
-    let rotated90 = rotate(grid, 1);
-    let visible_from_bottom = rotate(visibility_from_left(&rotated90), 3);
-
-    let rotated180 = rotate(rotated90, 1);
-    let visible_from_right = rotate(visibility_from_left(&rotated180), 2);
-
-    let rotated270 = rotate(rotated180, 1);
-    let visible_from_top = rotate(visibility_from_left(&rotated270), 1);
-
-    let part_1_answer = visible_from_left
-        .into_iter()
-        .enumerate()
-        .fold(0, |acc, (row_idx, row)| {
-            acc + row
+    let visibility_grid = (0..4)
+        .map(|turns| rotate(&visibility_from_left(&rotate(&grid, turns)), 4 - turns))
+        .reduce(|overall_vis_grid, side_vis_grid| {
+            overall_vis_grid
                 .into_iter()
                 .enumerate()
-                .filter(|(col_idx, visible_left)| {
-                    *visible_left
-                        || visible_from_bottom[row_idx][*col_idx]
-                        || visible_from_right[row_idx][*col_idx]
-                        || visible_from_top[row_idx][*col_idx]
+                .map(|(row_idx, row)| {
+                    row.into_iter()
+                        .enumerate()
+                        .map(|(col_idx, val)| val || side_vis_grid[row_idx][col_idx])
+                        .collect()
                 })
-                .count()
-        });
+                .collect()
+        })
+        .unwrap();
 
+    let part_1_answer = visibility_grid.into_iter().flatten().filter(|b| *b).count();
     println!("part 1: {}", part_1_answer);
 }
