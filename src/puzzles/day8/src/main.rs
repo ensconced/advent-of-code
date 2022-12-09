f5d400ee02e1c70b6674660dfc2b28c0e2b51d4b
Joe Barnett <joebarnett876@gmail.com>
2022-12-09 08:45:03 +0000

    wip

--- src/puzzles/day8/src/main.rs
+++ src/puzzles/day8/src/main.rs
@@ -1,25 +1,46 @@
 use utils::read_input;
 
-type Matrix<T> = Vec<Vec<T>>;
+#[derive(Clone)]
+struct Matrix<T>(Vec<Vec<T>>);
 
-fn transpose<T: Copy>(grid: Matrix<T>) -> Matrix<T> {
-    grid.iter()
-        .enumerate()
-        .map(|(row_idx, row)| {
-            (0..row.len())
-                .map(|col_idx| grid[col_idx][row_idx])
-                .collect()
+impl<T: Copy> Matrix<T> {
+    fn transpose(&self) -> Matrix<T> {
+        Matrix(
+            self.0
+                .iter()
+                .enumerate()
+                .map(|(row_idx, row)| {
+                    (0..row.len())
+                        .map(|col_idx| self.0[col_idx][row_idx])
+                        .collect()
+                })
+                .collect(),
+        )
+    }
+
+    fn rotate(&self, n: u32) -> Matrix<T> {
+        (0..n % 4).fold(self.clone(), |acc, _| {
+            Matrix(
+                acc.transpose()
+                    .rows()
+                    .map(|row| row.into_iter().rev().collect())
+                    .collect(),
+            )
         })
-        .collect()
-}
+    }
 
-fn rotate<T: Copy>(grid: &Matrix<T>, n: u32) -> Matrix<T> {
-    (0..n).fold(grid.clone(), |acc, _| {
-        transpose(acc)
-            .into_iter()
-            .map(|row| row.into_iter().rev().collect())
-            .collect()
-    })
+    fn transform_lines<S: Copy, F: Fn(&Vec<T>) -> Vec<S>>(
+        &self,
+        line_direction: u32,
+        transform: F,
+    ) -> Matrix<S> {
+        let turns = line_direction % 4;
+        Matrix(self.rotate(turns).0.iter().map(transform).collect()).rotate(4 - turns)
+    }
+
+    fn rows(self) -> impl Iterator<Item = Vec<T>> {
+        self.0.into_iter()
+    }
 }
 
 fn max_blocking_tree_heights(line: &Vec<i32>) -> Vec<i32> {
@@ -32,46 +53,49 @@ fn max_blocking_tree_heights(line: &Vec<i32>) -> Vec<i32> {
     result
 }
 
-fn visibility_from_left(grid: &Matrix<i32>) -> Matrix<bool> {
-    let blockers: Vec<_> = grid.iter().map(max_blocking_tree_heights).collect();
-    grid.iter()
-        .enumerate()
-        .map(|(row_idx, row)| {
-            row.iter()
-                .enumerate()
-                .map(|(col_idx, tree)| *tree > blockers[row_idx][col_idx])
-                .collect()
-        })
-        .collect()
+fn visibility_from_side(line_direction: u32, grid: &Matrix<i32>) -> Matrix<bool> {
+    grid.transform_lines(line_direction, |row| {
+        let blockers = max_blocking_tree_heights(row);
+        row.iter()
+            .enumerate()
+            .map(|(col_idx, tree)| *tree > blockers[col_idx])
+            .collect()
+    })
 }
 
 fn main() {
     let input = read_input();
-    let grid = input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|ch| char::to_digit(ch, 10).unwrap() as i32)
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
+    let grid = Matrix(
+        input
+            .lines()
+            .map(|line| {
+                line.chars()
+                    .map(|ch| char::to_digit(ch, 10).unwrap() as i32)
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>(),
+    );
 
     let visibility_grid = (0..4)
-        .map(|turns| rotate(&visibility_from_left(&rotate(&grid, turns)), 4 - turns))
+        .map(|line_direction| visibility_from_side(line_direction, &grid))
         .reduce(|overall_vis_grid, side_vis_grid| {
-            overall_vis_grid
-                .into_iter()
-                .enumerate()
-                .map(|(row_idx, row)| {
-                    row.into_iter()
-                        .enumerate()
-                        .map(|(col_idx, val)| val || side_vis_grid[row_idx][col_idx])
-                        .collect()
-                })
-                .collect()
+            Matrix(
+                overall_vis_grid
+                    .rows()
+                    .enumerate()
+                    .map(|(row_idx, row)| {
+                        row.into_iter()
+                            .enumerate()
+                            .map(|(col_idx, val)| val || side_vis_grid.0[row_idx][col_idx])
+                            .collect()
+                    })
+                    .collect(),
+            )
         })
         .unwrap();
 
-    let part_1_answer = visibility_grid.into_iter().flatten().filter(|b| *b).count();
+    let part_1_answer = visibility_grid.rows().flatten().filter(|b| *b).count();
     println!("part 1: {}", part_1_answer);
+
+    // TODO - use binary search tree for part 2, to look up latest idx per line of a tree of matching or greater height...
 }
