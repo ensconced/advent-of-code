2dbe97fb59f1ed727ad563ebcb27c61b3abee3b6
Joe Barnett <joebarnett876@gmail.com>
2022-12-13 07:50:51 +0000

    finish day11

--- src/puzzles/day11/src/main.rs
+++ src/puzzles/day11/src/main.rs
@@ -35,23 +35,24 @@ fn take_compute_new_worry_level<'a>(
         .next()
         .map(|line| {
             let line = line.to_owned();
-            Box::new(move |old_worry_level: i64| {
-                let mut parts = line
-                    .trim()
-                    .strip_prefix("Operation: new = ")
-                    .unwrap()
-                    .split(' ');
+            let parts: Vec<_> = line
+                .trim()
+                .strip_prefix("Operation: new = ")
+                .unwrap()
+                .split(' ')
+                .map(|part| part.to_owned())
+                .collect();
 
-                let lhs = match parts.next().unwrap() {
+            Box::new(move |old_worry_level: i64| {
+                let lhs = match parts[0].as_str() {
                     "old" => old_worry_level,
                     other_str => str::parse::<i64>(other_str).unwrap(),
                 };
-                let operation = parts.next().unwrap();
-                let rhs = match parts.next().unwrap() {
+                let rhs = match parts[2].as_str() {
                     "old" => old_worry_level,
                     other_str => str::parse::<i64>(other_str).unwrap(),
                 };
-                match operation {
+                match parts[1].as_str() {
                     "*" => lhs * rhs,
                     "+" => lhs + rhs,
                     _ => panic!(),
@@ -123,7 +124,28 @@ fn parse_monkeys(input: String) -> Vec<Monkey> {
         .collect()
 }
 
-fn monkey_business_level(mut monkeys: Vec<Monkey>) -> u64 {
+fn monkey_business_level<F: Fn(i64) -> i64>(
+    mut monkeys: Vec<Monkey>,
+    rounds: u32,
+    worry_level_adjustment: F,
+) -> u64 {
+    (0..rounds).for_each(|_| {
+        (0..monkeys.len()).for_each(|monkey_idx| {
+            while !monkeys[monkey_idx].items.is_empty() {
+                monkeys[monkey_idx].activity += 1;
+                let item = monkeys[monkey_idx].items.remove(0);
+                let new_worry_level =
+                    worry_level_adjustment((monkeys[monkey_idx].compute_new_worry_level)(item));
+                let next_monkey = if new_worry_level % monkeys[monkey_idx].test_divisor == 0 {
+                    monkeys[monkey_idx].next_monkey_if_true
+                } else {
+                    monkeys[monkey_idx].next_monkey_if_false
+                };
+                monkeys[next_monkey].items.push(new_worry_level);
+            }
+        });
+    });
+
     monkeys.sort_by_key(|monkey| monkey.activity);
     monkeys.reverse();
     monkeys
@@ -134,24 +156,16 @@ fn monkey_business_level(mut monkeys: Vec<Monkey>) -> u64 {
 }
 
 fn main() {
-    let mut monkeys = parse_monkeys(read_input());
-    let divisor_product: i64 = monkeys.iter().map(|monkey| monkey.test_divisor).product();
+    let monkeys = parse_monkeys(read_input());
+    println!(
+        "part 1: {}",
+        monkey_business_level(monkeys, 20, |worry_level| worry_level / 3)
+    );
 
-    (0..10000).for_each(|_| {
-        (0..monkeys.len()).for_each(|monkey_idx| {
-            while !monkeys[monkey_idx].items.is_empty() {
-                monkeys[monkey_idx].activity += 1;
-                let item = monkeys[monkey_idx].items.remove(0);
-                let new_worry_level =
-                    (monkeys[monkey_idx].compute_new_worry_level)(item) % divisor_product;
-                let next_monkey = if new_worry_level % monkeys[monkey_idx].test_divisor == 0 {
-                    monkeys[monkey_idx].next_monkey_if_true
-                } else {
-                    monkeys[monkey_idx].next_monkey_if_false
-                };
-                monkeys[next_monkey].items.push(new_worry_level);
-            }
-        });
-    });
-    println!("part 1: {}", monkey_business_level(monkeys));
+    let monkeys = parse_monkeys(read_input());
+    let divisor_product: i64 = monkeys.iter().map(|monkey| monkey.test_divisor).product();
+    println!(
+        "part 2: {}",
+        monkey_business_level(monkeys, 10000, |worry_level| worry_level % divisor_product)
+    );
 }
