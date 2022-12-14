28ddbb307478fc59cec67506123a54fe18813367
Joe Barnett <joebarnett876@gmail.com>
2022-12-14 21:20:12 +0000

    wip

--- src/puzzles/day14/src/main.rs
+++ src/puzzles/day14/src/main.rs
@@ -3,27 +3,27 @@ use std::collections::HashSet;
 use itertools::Itertools;
 use utils::read_input;
 
-fn add_vertical_section(x: u32, y1: u32, y2: u32, rocks: &mut HashSet<(u32, u32)>) {
+fn add_vertical_section(x: i32, y1: i32, y2: i32, rocks: &mut HashSet<(i32, i32)>) {
     for y in y1..=y2 {
         rocks.insert((x, y));
     }
 }
 
-fn add_horizontal_section(y: u32, x1: u32, x2: u32, rocks: &mut HashSet<(u32, u32)>) {
+fn add_horizontal_section(y: i32, x1: i32, x2: i32, rocks: &mut HashSet<(i32, i32)>) {
     for x in x1..=x2 {
         rocks.insert((x, y));
     }
 }
 
-fn get_rock_positions() -> HashSet<(u32, u32)> {
+fn get_rock_positions() -> HashSet<(i32, i32)> {
     let input = read_input();
     let mut rocks = HashSet::new();
     for line in input.lines() {
         line.split(" -> ")
             .map(|section| {
                 let coords: Vec<_> = section.split(',').collect();
-                let x: u32 = str::parse(coords[0]).unwrap();
-                let y: u32 = str::parse(coords[1]).unwrap();
+                let x: i32 = str::parse(coords[0]).unwrap();
+                let y: i32 = str::parse(coords[1]).unwrap();
                 (x, y)
             })
             .tuple_windows()
@@ -31,15 +31,15 @@ fn get_rock_positions() -> HashSet<(u32, u32)> {
                 if section_start.0 == section_end.0 {
                     add_vertical_section(
                         section_start.0,
-                        u32::min(section_start.1, section_end.1),
-                        u32::max(section_start.1, section_end.1),
+                        i32::min(section_start.1, section_end.1),
+                        i32::max(section_start.1, section_end.1),
                         &mut rocks,
                     );
                 } else if section_start.1 == section_end.1 {
                     add_horizontal_section(
                         section_start.1,
-                        u32::min(section_start.0, section_end.0),
-                        u32::max(section_start.0, section_end.0),
+                        i32::min(section_start.0, section_end.0),
+                        i32::max(section_start.0, section_end.0),
                         &mut rocks,
                     );
                 } else {
@@ -50,15 +50,22 @@ fn get_rock_positions() -> HashSet<(u32, u32)> {
     rocks
 }
 
-fn count_settled_grains(mut rocks: HashSet<(u32, u32)>) -> usize {
+fn count_settled_grains(mut rocks: HashSet<(i32, i32)>, include_floor: bool) -> usize {
     let rocks_max_y = rocks.iter().max_by_key(|(_, y)| y).unwrap().1;
     let mut settled_sand_grains = 0;
 
     loop {
-        let mut sand_position = (500, 0);
+        let mut sand_position = (500, -1);
         loop {
-            if sand_position.1 > rocks_max_y {
+            if !include_floor && sand_position.1 > rocks_max_y {
                 return settled_sand_grains;
+            } else if include_floor && sand_position.1 == rocks_max_y + 1 {
+                rocks.insert(sand_position);
+                settled_sand_grains += 1;
+                if sand_position == (500, 0) {
+                    return settled_sand_grains;
+                }
+                break;
             } else if !rocks.contains(&(sand_position.0, sand_position.1 + 1)) {
                 sand_position = (sand_position.0, sand_position.1 + 1);
             } else if !rocks.contains(&(sand_position.0 - 1, sand_position.1 + 1)) {
@@ -68,6 +75,9 @@ fn count_settled_grains(mut rocks: HashSet<(u32, u32)>) -> usize {
             } else {
                 rocks.insert(sand_position);
                 settled_sand_grains += 1;
+                if sand_position == (500, 0) {
+                    return settled_sand_grains;
+                }
                 break;
             }
         }
@@ -76,6 +86,10 @@ fn count_settled_grains(mut rocks: HashSet<(u32, u32)>) -> usize {
 
 fn main() {
     let rocks = get_rock_positions();
-    let part_1_answer = count_settled_grains(rocks);
+    let part_1_answer = count_settled_grains(rocks, false);
     println!("part 1: {}", part_1_answer);
+
+    let rocks = get_rock_positions();
+    let part_2_answer = count_settled_grains(rocks, true);
+    println!("part 2: {}", part_2_answer);
 }
