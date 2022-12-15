560452b340c1aac01179177c454d71546daeceb5
Joe Barnett <joebarnett876@gmail.com>
2022-12-15 22:24:58 +0000

    wip

--- src/puzzles/day15/src/main.rs
+++ src/puzzles/day15/src/main.rs
@@ -1,16 +1,16 @@
-use gcollections::ops::{Cardinality, Difference, Empty, Union};
+use gcollections::ops::{Bounded, Cardinality, Difference, Empty, Union};
 use interval::{interval_set::ToIntervalSet, IntervalSet};
 use utils::read_input;
 
 #[derive(Debug)]
 struct Coordinates {
-    x: i32,
-    y: i32,
+    x: i64,
+    y: i64,
 }
 
 impl Coordinates {
-    fn manhattan_distance(&self, other: &Self) -> i32 {
-        i32::abs(self.x - other.x) + i32::abs(self.y - other.y)
+    fn manhattan_distance(&self, other: &Self) -> i64 {
+        i64::abs(self.x - other.x) + i64::abs(self.y - other.y)
     }
 }
 
@@ -20,14 +20,14 @@ struct Sensor {
     closest_beacon: Coordinates,
 }
 
-fn remove(interval_set: IntervalSet<i32>, element: i32) -> IntervalSet<i32> {
+fn remove(interval_set: IntervalSet<i64>, element: i64) -> IntervalSet<i64> {
     interval_set.difference(&vec![(element, element)].to_interval_set())
 }
 
 impl Sensor {
-    fn exclusion_zone_at_y(&self, y: i32) -> IntervalSet<i32> {
+    fn exclusion_zone_at_y(&self, y: i64) -> IntervalSet<i64> {
         let manhattan_distance = self.coordinates.manhattan_distance(&self.closest_beacon);
-        let vertical_distance = i32::abs(self.coordinates.y - y);
+        let vertical_distance = i64::abs(self.coordinates.y - y);
         if vertical_distance >= manhattan_distance {
             IntervalSet::empty()
         } else {
@@ -50,23 +50,23 @@ fn parse_sensor(line: &str) -> Sensor {
         .unwrap()
         .strip_suffix(',')
         .unwrap()
-        .parse::<i32>()
+        .parse::<i64>()
         .unwrap();
     let sensor_y = parts[3]
         .strip_prefix("y=")
         .unwrap()
         .strip_suffix(':')
         .unwrap()
-        .parse::<i32>()
+        .parse::<i64>()
         .unwrap();
     let beacon_x = parts[8]
         .strip_prefix("x=")
         .unwrap()
         .strip_suffix(',')
         .unwrap()
-        .parse::<i32>()
+        .parse::<i64>()
         .unwrap();
-    let beacon_y = parts[9].strip_prefix("y=").unwrap().parse::<i32>().unwrap();
+    let beacon_y = parts[9].strip_prefix("y=").unwrap().parse::<i64>().unwrap();
     Sensor {
         coordinates: Coordinates {
             x: sensor_x,
@@ -79,7 +79,7 @@ fn parse_sensor(line: &str) -> Sensor {
     }
 }
 
-fn exclusion_zone(sensors: &[Sensor], y: i32) -> IntervalSet<i32> {
+fn exclusion_zone(sensors: &[Sensor], y: i64) -> IntervalSet<i64> {
     sensors.iter().fold(IntervalSet::empty(), |acc, sensor| {
         let mut exclusion_zone = sensor.exclusion_zone_at_y(y);
         if sensor.closest_beacon.y == y {
@@ -95,4 +95,13 @@ fn main() {
 
     let part_1_answer = exclusion_zone(&sensors, 2000000).size();
     println!("part 1: {}", part_1_answer);
+
+    let all_col_idxs = vec![(0, 4000000)].to_interval_set();
+    // part 2
+    (0..4000000).for_each(|y| {
+        let possible_locations = all_col_idxs.difference(&exclusion_zone(&sensors, y));
+        if let Some(x) = possible_locations.into_iter().next() {
+            println!("{}", x.lower() * 4000000 + y);
+        }
+    });
 }
