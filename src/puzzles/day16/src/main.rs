8602627981bc73922d2aba44595461f3a9249d2e
Joe Barnett <joebarnett876@gmail.com>
2022-12-18 11:16:45 +0000

    wip

--- src/puzzles/day16/src/main.rs
+++ src/puzzles/day16/src/main.rs
@@ -35,7 +35,7 @@ struct ValvePath<'a> {
 }
 
 impl<'a> ValvePath<'a> {
-    fn new(start_valve: &'a Valve) -> Self {
+    fn initialise(start_valve: &'a Valve) -> Self {
         Self {
             steps_since_opening_valve: 0,
             prev_steps: vec![],
@@ -44,6 +44,51 @@ impl<'a> ValvePath<'a> {
             done: false,
             score: 0,
             minute: 0,
+        }
+    }
+
+    fn do_nothing(self) -> ValvePath<'a> {
+        ValvePath {
+            steps_since_opening_valve: self.steps_since_opening_valve,
+            prev_steps: self.prev_steps,
+            current_valve: self.current_valve,
+            open_valves: self.open_valves,
+            done: true,
+            score: self.score,
+            minute: self.minute + 1,
+        }
+    }
+
+    fn move_to_valve(
+        &self,
+        valve: &str,
+        valve_lookup: &'a HashMap<&'a str, Valve>,
+    ) -> ValvePath<'a> {
+        let mut prev_steps = self.prev_steps.clone();
+        prev_steps.push(self.current_valve.name);
+
+        ValvePath {
+            steps_since_opening_valve: self.steps_since_opening_valve + 1,
+            prev_steps,
+            current_valve: valve_lookup.get(valve).unwrap(),
+            open_valves: self.open_valves.clone(),
+            done: false,
+            score: self.score,
+            minute: self.minute + 1,
+        }
+    }
+
+    fn open_valve(mut self, minute: u32) -> ValvePath<'a> {
+        let score = self.score + self.current_valve.flow_rate * (MINUTES - minute);
+        self.open_valves.insert(self.current_valve.name);
+        ValvePath {
+            steps_since_opening_valve: 0,
+            prev_steps: self.prev_steps,
+            current_valve: self.current_valve,
+            open_valves: self.open_valves,
+            done: false,
+            score,
+            minute: self.minute + 1,
         }
     }
 
@@ -68,60 +113,32 @@ impl<'a> ValvePath<'a> {
                 .current_valve
                 .neighbours
                 .iter()
-                .map(|neighbour| {
-                    let mut prev_steps = self.prev_steps.clone();
-                    prev_steps.push(self.current_valve.name);
-                    ValvePath {
-                        steps_since_opening_valve: self.steps_since_opening_valve + 1,
-                        prev_steps,
-                        current_valve: valve_lookup.get(neighbour).unwrap(),
-                        open_valves: self.open_valves.clone(),
-                        done: false,
-                        score: self.score,
-                        minute: self.minute + 1,
-                    }
-                })
+                .map(|neighbour| self.move_to_valve(neighbour, valve_lookup))
                 .filter(|path| !path.ends_with_pointless_cycle())
                 .collect();
 
             if self.open_valves.contains(self.current_valve.name) {
-                extended_paths.push(ValvePath {
-                    steps_since_opening_valve: self.steps_since_opening_valve,
-                    prev_steps: self.prev_steps,
-                    current_valve: self.current_valve,
-                    open_valves: self.open_valves,
-                    done: true,
-                    score: self.score,
-                    minute: self.minute + 1,
-                });
+                extended_paths.push(self.do_nothing());
             } else {
-                let score = self.score + self.current_valve.flow_rate * (MINUTES - minute);
-                self.open_valves.insert(self.current_valve.name);
-                extended_paths.push(ValvePath {
-                    steps_since_opening_valve: 0,
-                    prev_steps: self.prev_steps,
-                    current_valve: self.current_valve,
-                    open_valves: self.open_valves,
-                    done: false,
-                    score,
-                    minute: self.minute + 1,
-                });
+                extended_paths.push(self.open_valve(minute));
             }
             extended_paths
         }
     }
 
     fn final_score_upper_bound(
-        &self,
+        current_valve_name: &str,
+        open_valves: &HashSet<&str>,
         valve_lookup: &'a HashMap<&'a str, Valve>,
         shortest_paths: &SortedShortestPaths,
+        current_score: u32,
         minute: u32,
     ) -> u32 {
         let upper_bound = shortest_paths
-            .all_shortest_paths_from(self.current_valve.name)
+            .all_shortest_paths_from(current_valve_name)
             .unwrap()
             .iter()
-            .filter(|(valve_name, _)| !self.open_valves.contains(*valve_name))
+            .filter(|(valve_name, _)| !open_valves.contains(*valve_name))
             .enumerate()
             .map(|(idx, (valve_name, path_length))| {
                 let min_minute_to_open_valve = minute + path_length + 1 + idx as u32;
@@ -130,7 +147,7 @@ impl<'a> ValvePath<'a> {
                 flow_rate * max_minutes_of_flow
             })
             .sum::<u32>();
-        upper_bound + self.score
+        upper_bound + current_score
     }
 }
 
@@ -152,7 +169,7 @@ struct PathCollection<'a> {
 
 impl<'a> PathCollection<'a> {
     fn new(start_valve: &'a Valve) -> Self {
-        let path = ValvePath::new(start_valve);
+        let path = ValvePath::initialise(start_valve);
         let score = path.score;
         Self {
             paths: vec![path],
@@ -170,8 +187,14 @@ impl<'a> PathCollection<'a> {
         for old_path in old_paths.into_iter() {
             let extended_paths = old_path.all_possible_extensions(minute, valve_lookup);
             for extended_path in extended_paths {
-                let score_upper_bound =
-                    extended_path.final_score_upper_bound(valve_lookup, shortest_paths, minute);
+                let score_upper_bound = ValvePath::final_score_upper_bound(
+                    extended_path.current_valve.name,
+                    &extended_path.open_valves,
+                    valve_lookup,
+                    shortest_paths,
+                    extended_path.score,
+                    minute,
+                );
 
                 if score_upper_bound > self.max_score {
                     let extended_path_score = extended_path.score;
